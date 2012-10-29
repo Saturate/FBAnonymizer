ca1a4dbd56e0550b4694a4cd497cc137ffeb8e41
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2012-10-29 19:33:25 +0100

    Added first version
    Added the first version, mostly a POC. A lot of changes are coming.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-FBAnonymizer
-============
-
+#FBAnonymizer
 FBAnonymizer blurs names on your facebook with one click. Makes it easy to take screenshots without sharing any private infomation.
+
+##How To Install
+I'll try to add it to the Google Chrome Store. Until then you need to install it some other way.
+
+####Install with crx file:
+
+- Open "chrome://chrome/extensions/" in a tab.
+- Drag the FBAnonymizer.crx to this page.
+
+If you don't do this, you will get a popup that says you need to install it from the Chrome Store.
+
+##How To Use
+Just click the icon while on a FB tab. If names are not blured, update chrome. Support for older versions is coming later along with blackbar sensoring, instead of blur.
+
+##Todo
+- Options Pag
+- Better Logic
+
+##Contribute
+Just fork it and make a pull-request! Or if you have any ideas, make a issue!
---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// By Allan Kimmer Jensen with help from the example here:
+// http://developer.chrome.com/trunk/extensions/samples.html#cc8563a6666add797264184a960c7b7c8bd3e64d
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.windows.getCurrent(function(win) {
+    chrome.tabs.getSelected(win.id, actionClicked);
+  });
+});
+
+var version = "1.0";
+
+var cssstyle = ".fbxWelcomeBox .fbxWelcomeBoxName { -webkit-filter: blur(10px); }";
+
+function actionClicked(tab) {
+	var tabUrl = tab.url;
+	if( tabUrl.indexOf("facebook.com") != -1){
+		chrome.tabs.executeScript(null, {code:"document.body.bgColor='red'"});
+
+	chrome.tabs.insertCSS(null, {file: 'css/inject.css'}, function() { });
+
+	} else {
+		console.log('This is not FB!');
+	}
+}
+
+/*
+	Google Analytics for tracking use
+	More infomation: https://developer.chrome.com/trunk/extensions/tut_analytics.html
+	If you want access to the data just send me a message!
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-34374614-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
